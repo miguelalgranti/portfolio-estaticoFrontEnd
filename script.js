f3a236cd37f3d6511a8b375d3674c0907b13ec45
miguelalgranti <108410813+miguelalgranti@users.noreply.github.com>
2022-07-26 10:19:54 -0300

    version nueva

---
+++ script.js
@@ -0,0 +1,8 @@
+function cambiar_parrafo(){
+    document.getElementById("edit-acercade").style.display="block";
+    let texto = document.getElementById("text-acercade").innerText;
+    console.log(texto); 
+};
+function myfunction2(valor){
+    document.getElementById("text-acercade").innerText=valor
+}; 
